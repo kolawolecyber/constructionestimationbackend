46d040c4270b5671b4e52bfdafabdf2ef2b846a2
Abdulakeem abdulafeez Kolawole <kolawolecyber@users.noreply.github.com>
2025-07-27 16:32:12 +0800

    UPDATE App.js

--- App.js
+++ App.js
@@ -33,5 +33,6 @@ app.use(session({
 app.use(passport.session());
 
 app.use('/api/auth', authRoutes);
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(PORT, () => console.log('Server running on port 5000'));
